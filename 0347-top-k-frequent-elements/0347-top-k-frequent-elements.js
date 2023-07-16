29fbc528d3a66cf2a4cfb35c72b35bba39ad64d3
Hafizh Maulana <63697314+hafizhmaul@users.noreply.github.com>
2023-07-16 14:16:07 +0700

    Time: 64 ms (94.49%), Space: 44.1 MB (95.72%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -4,21 +4,25 @@
  * @return {number[]}
  */
 var topKFrequent = function(nums, k) {
+  //buat object Map untuk menampung value
   let map = new Map();
   
   for(let i = 0; i < nums.length; i++){
     if(map.has(nums[i])){
+      //kalo sudah pernah ada key dari nums[i] sebelumnya, maka increment valuenya 
       let currentValue = map.get(nums[i]);
       let incrementedValue = currentValue + 1;
       
       map.set(nums[i], incrementedValue)
     } else {
+      //kalo key nya belum pernah ada, tambahin key nya kedalam object map dan beri value 1
       map.set(nums[i], 1)
     }
   }
+
+  // ubah object map menjadi sebuah array dan urutkan berdasarkan dari terbesar ke terkecil menggunakan valuenya
+  let sortedEntries = [...map.entries()].sort((a,b) => b[1]-a[1]);
   
-  let entries = Object.entries([...map]);
-  let sortObject = Object.values(Object.fromEntries(entries)).sort((a,b) => b[1] - a[1]);
-  
-  return sortObject.map(([firstValue]) => firstValue).slice(0,k)
+    //potong array sebanyak jumlah inputan K, dan buat array baru menggunakan method array map, yang dimana array nya berisi keynya
+  return sortedEntries.slice(0, k).map(([key]) => key);
 };
