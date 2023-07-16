c2f8ace0f073bc7610b420834ffe08bd447bd64a
Hafizh Maulana <63697314+hafizhmaul@users.noreply.github.com>
2023-07-16 19:13:18 +0700

    Time: 61 ms (73.68%), Space: 45.2 MB (35.70%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -4,20 +4,20 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    let left = 0;
-    let right = nums.length-1;
+    let left = 0; //tentukan index paling awal
+    let right = nums.length-1; //tentukan index paling akhir
     
-    while(left <= right){
-        let mid = Math.floor((left + right) / 2);
+    while(left <= right){ //selama index paling awal kurang atau sama dengan index paling akhir lakukan operasi ini
+        let mid = Math.floor((left + right) / 2); //ambil index tengah dari array nums
         
-        if(nums[mid] === target){
+        if(nums[mid] === target){ //kalo nums[mid] sama dengan target kembalikan nilai indexnya
             return mid;
-        } else if(nums[mid] < target){
+        } else if(nums[mid] < target){ //kalo nums[mid] kurang dari nilai target, maka mid ditambah 1
             left = mid + 1;
-        } else {
-            right = mid - 1;
+        } else { //sebaliknya dari atas, maka mid dikurangi 1
+            right = mid - 1; ;
         }
     }
     
-    return -1;
+    return -1; //kalau target gak ada, kembalikan value ini
 };
