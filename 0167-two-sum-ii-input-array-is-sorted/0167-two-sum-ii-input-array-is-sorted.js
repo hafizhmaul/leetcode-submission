80f1d2782ed631cecb449e572276f436d74663fa
Hafizh Maulana <63697314+hafizhmaul@users.noreply.github.com>
2023-07-19 13:44:07 +0700

    Time: 62 ms (66.03%), Space: 42.8 MB (54.55%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -4,19 +4,19 @@
  * @return {number[]}
  */
 
-//karena arraynya sudah terurut, jadi bisa pakai binary search
-
 var twoSum = function(numbers, target) {
     let left = 0, right = numbers.length - 1;
   
     while(left < right){
-      let sum = numbers[left] + numbers[right];
-      if(sum == target){
-        return [left+1, right+1]
-      } else if(sum > target){
-        right--;
-      } else {
-        left++;
-      }
+        let sum = numbers[left] + numbers[right];
+        if(sum == target){
+            return [left+1, right+1]
+        } else if (sum < target){
+            left++;
+        } else {
+            right--;
+        }
     }
+    
+    return [-1,-1];
 };
