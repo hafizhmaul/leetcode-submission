fccb240d094f46a296640dcea99f6f3a01665d62
Hafizh Maulana <63697314+hafizhmaul@users.noreply.github.com>
2023-07-31 23:25:49 +0700

    Time: 61 ms (76.07%), Space: 44.2 MB (45.87%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.js
+++ 0137-single-number-ii/0137-single-number-ii.js
@@ -3,5 +3,15 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    return nums.filter((item, index, arr) => arr.indexOf(item) === index && arr.lastIndexOf(item) === index);
+    let map = new Map();
+  
+    for(let i = 0; i < nums.length; i++){
+      if(map.has(nums[i])){
+        map.set(nums[i], map.get(nums[i]) + 1);
+      } else {
+        map.set(nums[i], 1);
+      }
+    }
+  
+  return [...map].sort((a,b) => a[1] - b[1])[0][0];
 };
