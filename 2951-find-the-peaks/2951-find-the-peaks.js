c4cabb6b4a19db4e0908bb096115aa21f034326a
Hafizh Maulana <63697314+hafizhmaul@users.noreply.github.com>
2024-02-04 19:28:40 +0700

    Time: 61 ms (60.34%), Space: 52.2 MB (6.05%) - LeetHub

---
+++ 2951-find-the-peaks/2951-find-the-peaks.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} mountain
+ * @return {number[]}
+ */
+var findPeaks = function(mountain) {
+    let arr = [];
+   
+   for(let i = 0; i < mountain.length; i++){
+      if(mountain[i] > mountain[i+1] && mountain[i] > mountain[i-1]){
+         arr.push(i)
+      }
+   }
+   
+   return arr;
+};
